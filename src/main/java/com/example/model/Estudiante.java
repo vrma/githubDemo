e8e6a9c9d493f882bd4a92240e2b2c5c9d6e6d3c
dianaagustinaf <dianaagustinaf@gmail.com>
2022-05-18 14:48:06 +0200

    clase Estudiante

---
+++ src/main/java/com/example/model/Estudiante.java
@@ -0,0 +1,27 @@
+package com.example.model;
+
+public class Estudiante {
+
+    private String clase;
+
+    private String profesor;
+
+    public String getClase() {
+        return clase;
+    }
+
+    public void setClase(String clase) {
+        this.clase = clase;
+    }
+
+    public String getProfesor() {
+        return profesor;
+    }
+
+    public void setProfesor(String profesor) {
+        this.profesor = profesor;
+    }
+
+    
+    
+}
