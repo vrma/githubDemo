5cfd256903a827e0acc114c99b10eaec9488f67c
Stmarco <S.tmarco@alumnos.upm.es>
2022-05-18 14:49:40 +0200

    Clase Santiago

---
+++ src/main/java/com/example/model/claseSantiago.java
@@ -0,0 +1,28 @@
+package com.example.model;
+
+public class claseSantiago {
+   private int id;
+   private int edad;
+   private String nombre;
+   
+public int getId() {
+    return id;
+}
+public void setId(int id) {
+    this.id = id;
+}
+public int getEdad() {
+    return edad;
+}
+public void setEdad(int edad) {
+    this.edad = edad;
+}
+public String getNombre() {
+    return nombre;
+}
+public void setNombre(String nombre) {
+    this.nombre = nombre;
+}
+
+   
+}
