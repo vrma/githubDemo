a4bda323eb15c2e86271b2daffbc30238974fd6a
vrma <vrmachado@gmail.com>
2022-05-18 14:39:53 +0200

    campo primer apellido

--- src/main/java/com/example/model/Persona.java
+++ src/main/java/com/example/model/Persona.java
@@ -3,9 +3,26 @@ package com.example.model;
 public class Persona {
     
     private String nombre;
+    private String primerApellido;
 
     public Persona(String nombre) {
         this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPrimerApellido() {
+        return primerApellido;
+    }
+
+    public void setPrimerApellido(String primerApellido) {
+        this.primerApellido = primerApellido;
     }
 
     
